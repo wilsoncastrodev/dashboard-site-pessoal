a285001beb0310b22aec5d87ee3a24bc238d1c40
Wilson Castro da Paixão <contato@wilsoncastro.dev>
2023-02-24 13:27:21 -0300

    Adiciona filtro na Listagem de Categoria de Habilidades
    //Javascript Typescript React Redux HTML Bootstrap

--- src/components/category-skill/ListCategorySkill.tsx
+++ src/components/category-skill/ListCategorySkill.tsx
@@ -3,11 +3,12 @@ import { useAppDispatch, useAppSelector, RootState } from "../../stores/store";
 import { getAllCategorySkill, deleteCategorySkill } from "../../stores/features/categorySkillSlice";
 import { DataTable } from 'primereact/datatable';
 import { Column } from "primereact/column";
+import { InputText } from 'primereact/inputtext';
 import { MDCSnackbar } from '@material/snackbar';
+import { FilterMatchMode } from 'primereact/api';
 import EditFormCategorySkill from "./EditFormCategorySkill";
-import MediaQuery from 'react-responsive'
+import MediaQuery from 'react-responsive';
 import { Card } from "react-bootstrap";
-import { Image } from 'primereact/image';
 
 const ListCategorySkill: FC = () => {
     const user = useAppSelector((state: RootState) => state.auth.user);
@@ -16,6 +17,9 @@ const ListCategorySkill: FC = () => {
     const dispatch = useAppDispatch();
     const [expandedRows, setExpandedRows] = useState<any>(null);
     const [items, setItems] = useState<any>([]);
+    const [filters, setFilters] = useState<any>({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    });
 
     useEffect(() => {
         dispatch(getAllCategorySkill());
@@ -59,8 +63,24 @@ const ListCategorySkill: FC = () => {
         return (<button onClick={() => deleteItem(data)}><i className="fa-regular fa-trash-can"></i></button>);
     }
 
-    const imageTemplate = (data: any) => {
-        return (<Image src={data.image.url} alt={data.content} width="250" preview />)
+    const onGlobalFilterChange = (event: any) => {
+        const value = event.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+    };
+
+    const renderHeader = () => {
+        const value = filters['global'] ? filters['global'].value : '';
+
+        return (
+            <span className="p-input-icon-left">
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <InputText type="search" value={value || ''} onChange={(e) => onGlobalFilterChange(e)} placeholder="Pesquisar..." />
+            </span>
+        );
     };
 
     return (
@@ -68,8 +88,8 @@ const ListCategorySkill: FC = () => {
             {   items && items.length > 0 ?
                 <Fragment>
                     <MediaQuery minWidth={960}>
-                        <DataTable value={items} paginator rows={6} responsiveLayout="stack" className="mt-lg-5"
-                            expandedRows={expandedRows}
+                        <DataTable value={items} paginator rows={6} header={renderHeader()} filters={filters} responsiveLayout="stack"
+                            expandedRows={expandedRows} emptyMessage="Não foi encontrado nenhum resultado"
                             collapsedRowIcon={"fa-regular fa-pen-to-square"}
                             expandedRowIcon={"fa-solid fa-pen-to-square"}
                             onRowToggle={(e) => setExpandedRows(e.data)}
@@ -80,8 +100,8 @@ const ListCategorySkill: FC = () => {
                         </DataTable>
                     </MediaQuery>
                     <MediaQuery maxWidth={960}>
-                        <DataTable value={items} paginator rows={6} responsiveLayout="stack" className="mt-lg-5"
-                            expandedRows={expandedRows}
+                        <DataTable value={items} paginator rows={6} header={renderHeader()} filters={filters} responsiveLayout="stack"
+                            expandedRows={expandedRows} emptyMessage="Não foi encontrado nenhum resultado"
                             collapsedRowIcon={"fa-regular fa-pen-to-square"}
                             expandedRowIcon={"fa-solid fa-pen-to-square"}
                             onRowToggle={(e) => setExpandedRows(e.data)}
