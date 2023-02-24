97a3ca2e883aca8c3293bafefa218269a8705139
Wilson Castro da Paixão <contato@wilsoncastro.dev>
2023-02-24 13:34:42 -0300

    Adiciona filtro na Listagem de Conhecimentos Complementares
    //Javascript Typescript React Redux HTML Bootstrap

--- src/components/knowledge/ListKnowledge.tsx
+++ src/components/knowledge/ListKnowledge.tsx
@@ -4,7 +4,9 @@ import { getAllProfileKnowledge, deleteKnowledge } from "../../stores/features/k
 import { getAllCategoryKnowledge } from "../../stores/features/categoryKnowledgeSlice";
 import { DataTable } from 'primereact/datatable';
 import { Column } from "primereact/column";
+import { InputText } from 'primereact/inputtext';
 import { MDCSnackbar } from '@material/snackbar';
+import { FilterMatchMode } from 'primereact/api';
 import EditFormKnowledge from "./EditFormKnowledge";
 import MediaQuery from 'react-responsive';
 import { Card } from "react-bootstrap";
@@ -16,6 +18,9 @@ const ListKnowledge: FC = () => {
     const dispatch = useAppDispatch();
     const [expandedRows, setExpandedRows] = useState<any>(null);
     const [items, setItems] = useState<any>([]);
+    const [filters, setFilters] = useState<any>({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    });
 
     useEffect(() => {
         dispatch(getAllProfileKnowledge(user.profile._id));
@@ -60,13 +65,33 @@ const ListKnowledge: FC = () => {
         return (<button onClick={() => deleteItem(data)}><i className="fa-regular fa-trash-can"></i></button>);
     }
 
+    const onGlobalFilterChange = (event: any) => {
+        const value = event.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+    };
+
+    const renderHeader = () => {
+        const value = filters['global'] ? filters['global'].value : '';
+
+        return (
+            <span className="p-input-icon-left">
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <InputText type="search" value={value || ''} onChange={(e) => onGlobalFilterChange(e)} placeholder="Pesquisar..." />
+            </span>
+        );
+    };
+
     return (
         <Fragment>
             {   items && items.length > 0 ?
                 <Fragment>
                     <MediaQuery minWidth={960}>
-                        <DataTable value={items} paginator rows={6} responsiveLayout="stack" className="mt-lg-5"
-                            expandedRows={expandedRows}
+                        <DataTable value={items} paginator rows={6} header={renderHeader()} filters={filters} responsiveLayout="stack"
+                            expandedRows={expandedRows} emptyMessage="Não foi encontrado nenhum resultado"
                             collapsedRowIcon={"fa-regular fa-pen-to-square"}
                             expandedRowIcon={"fa-solid fa-pen-to-square"}
                             onRowToggle={(e) => setExpandedRows(e.data)}
@@ -78,8 +103,8 @@ const ListKnowledge: FC = () => {
                         </DataTable>
                     </MediaQuery>
                     <MediaQuery maxWidth={960}>
-                        <DataTable value={items} paginator rows={6} responsiveLayout="stack" className="mt-lg-5"
-                            expandedRows={expandedRows}
+                        <DataTable value={items} paginator rows={6} header={renderHeader()} filters={filters} responsiveLayout="stack"
+                            expandedRows={expandedRows} emptyMessage="Não foi encontrado nenhum resultado"
                             collapsedRowIcon={"fa-regular fa-pen-to-square"}
                             expandedRowIcon={"fa-solid fa-pen-to-square"}
                             onRowToggle={(e) => setExpandedRows(e.data)}
